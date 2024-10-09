60c68e5697b8b7a3e4a0b451fcf7c9e047ed7b65
kamal batesha <bateshakamal@gmail.com>
2024-10-09 15:36:37 +0300

    some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body className={`antialiased w-screen overflow-x-hidden`}>
         <Notification />
         <Navbar />
         {children}
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,16 +4,16 @@ import React from "react";
 
 export default function MenuPage() {
   return (
-    <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
+    <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] w-screen overflow-x-hidden flex flex-col md:flex-row items-center">
       {menu.map((category) => (
         <Link
           href={`/menu/${category.slug}`}
           key={category.id}
-          className="w-full  bg-cover p-8 flex-1 md:min-h-[50%]"
+          className="w-full h-1/3 md:w-1/3  bg-cover p-8 md:min-h-[50%]"
           style={{ backgroundImage: `url(${category.img})` }}
         >
           <div className={`text-${category.color} w-1/2 flex flex-col justify-between gap-8`}>
-            <h1 className="uppercase font-bold text-2xl sm:text-3xl text-nowrap">{category.title}</h1>
+            <h1 className="uppercase font-bold text-2xl sm:text-3xl ">{category.title}</h1>
             <p className="text-sm ">{category.desc}</p>
             <button
               className={`hidden 2xl:block ${
