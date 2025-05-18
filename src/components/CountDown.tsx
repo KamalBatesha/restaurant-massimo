f65879988b8fac8d8c9f87ba8effefcd3d5d5e4f
kamal batesha <109154159+KamalBatesha@users.noreply.github.com>
2025-05-18 19:22:37 +0300

    Update CountDown.tsx

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import Countdown from "react-countdown";
 
-const endTime = new Date("2024-12-28");
+const endTime = new Date("2025-12-28");
 
 export default function CountDown() {
   const [isMounted, setIsMounted] = useState(false);
